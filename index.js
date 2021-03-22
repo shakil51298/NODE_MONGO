8f3ac47ec2cf4971dabf27323d9ce57ce427cdd0
Md Khalid Hossain <zikaiwen02@gmail.com>
2021-03-23 01:44:39 +0800

    my first node project

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res)=>{
+    res.send('hellow World')
+})
+
+app.listen(3000, ()=>{
+    console.log("Listening to port 3000");
+})
